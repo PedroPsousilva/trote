f5dae60538c866a346bf902c26380779b02be0a8
PEDRO PAULO DE SOUSA SILVA <26894@unifacef.edu.br>
2026-04-08 21:51:35 -0300

    Finalizando calculadora

--- trote/script.js
+++ trote/script.js
@@ -1,46 +1,51 @@
-function calcular() {
-    //JS é uma linguagemnão tipada(sem tipo de dados)
-    //Number() é uma função que converte texto em numero
-    //Recupera o valor informado no elemento com id qtdeArroz
-    let qtdeArroz = Number(document.getElementById("qtdeArroz").value)
-    //Recupera o valor informado no elemento com id qtdeArroz
-    let qtdeFeijao = Number(document.getElementById("qtdeFeijao").value)
-    let qtdeOleo = Number(document.getElementById("qtdeOleo").value)
-    let qtdeMacarrao = Number(document.getElementById("qtdeMacarrao").value)
-    //calcula o valor total de pontos
-    let pontos = (qtdeArroz * 10) + (qtdeFeijao * 8) + (qtdeOleo * 4) + (qtdeMacarrao * 2)
-    //Vamos exibir o resultado para o usuario
-    document.getElementById("resultado").innerText = "Pontuação Total: " + pontos
+function calcular(){
+    // JS é uma linguagem não tipada (sem tipo de dados)
+    // Number() é uma função que converte texto em número
 
-    // Calcula o valor das metas
-    //Recupera a cor da equipe
+    /* recupera valores de itens avulsos */
+
+    // recupera o valor informado no elemento com id qtdeArroz
+    let qtdeArroz = Number(document.getElementById("qtdeArroz").value)
+    // recupera o valor informado no elemento com id qtdeFeijao
+    let qtdeFeijao = Number(document.getElementById("qtdeFeijao").value)
+    // recupera o valor informado no elemento com id qtdeOleo
+    let qtdeOleo = Number(document.getElementById("qtdeOleo").value)
+    // recupera o valor informado no elemento com id qtdeMacarrao
+    let qtdeMacarrao = Number(document.getElementById("qtdeMacarrao").value)
+    // calcula o valor total de pontos dos itens avulsos
+    let pontos = (qtdeArroz * 10) + (qtdeFeijao * 8) + (qtdeOleo * 4) + (qtdeMacarrao * 2)
+
+    // calcula o valor total das metas
+    // recupera a cor da equipe
     let cor = document.getElementById("cor").value
-    let metaProva1
-    if (cor == 'amarela') {
+    let metaProva1 // declara a variável
+    if (cor == "amarela"){
         metaProva1 = 54
     }
-    else if (cor == 'cinza') {
+    else if (cor == "cinza"){
         metaProva1 = 51
     }
-    else if (cor == 'laranja') {
+    else if (cor == "laranha"){
         metaProva1 = 21
     }
-    else if (cor == 'marrom') {
+    else if (cor == "marron"){
         metaProva1 = 88
     }
-    else if (cor == 'preta') {
+    else if (cor == "preta"){
         metaProva1 = 60
     }
 
-    alert(cor)
     let kitAlimentacao = Number(document.getElementById("kitAlimentacao").value)
     if (kitAlimentacao >= metaProva1) {
-        pontos = pontos + 5000 // Já garantiu o 5000 pontos
-        if (kitAlimentacao > 60) {
-            pontos = pontos + (5000 / 60) * (kitAlimentacao - 60) * 83.33
+        pontos = pontos + 5000 // já garantimos 5000
+        if (kitAlimentacao > metaProva1){
+            // ganhamos 83.33 a mais de ponto por itens que ultrapassaram 60
+            pontos = pontos + (kitAlimentacao - metaProva1) * (5000/metaProva1)
         }
     }
     else {
-        pontos = pontos + (5000 / 60) * (kitAlimentacao * 83.33)
+        pontos = pontos + kitAlimentacao * (5000 / metaProva1)
     }
+    // vamos exibir o resultado para o usuário
+    document.getElementById("resultado").innerText = "Pontuação total: " + pontos + " pontos"
 }
